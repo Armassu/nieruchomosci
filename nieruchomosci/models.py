a59cc79df247e510b57c03b6746c1fbf04b53af3
Paweł Byrka <gdx154315@student.gdansk.merito.pl>
2023-11-27 22:13:06 +0100

    first commit

---
+++ nieruchomosci/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class Nieruchomosc(models.Model):
+    tytul = models.CharField(max_length=200)
+    opis = models.TextField()
+    cena = models.DecimalField(max_digits=10, decimal_places=2)
+    metraz = models.DecimalField(max_digits=6, decimal_places=2)
+    liczba_pokoi = models.IntegerField()
+    zdjecie = models.ImageField(upload_to='zdjecia_nieruchomosci/')
+    data_dodania = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tytul
+
